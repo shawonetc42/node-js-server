37b9011c847bd07a54f3a0aa2c1e22df43d883cf
shawonetc42 <139037447+shawonetc42@users.noreply.github.com>
2024-04-28 12:07:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
